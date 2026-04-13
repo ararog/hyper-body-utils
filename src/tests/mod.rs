0ea0617be54df9150ae748b01f500864460c07ae
Rogério Araújo <rogerio.araujo@gmail.com>
2026-04-13 16:25:41 -0300

    refactor: added improvements to make crate more runtime agnostic

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,21 +1,19 @@
-use futures::StreamExt;
+use futures::{StreamExt, TryStreamExt};
+use hyper::body::Frame;
 #[cfg(feature = "smol-rt")]
 use macro_rules_attribute::apply;
 #[cfg(feature = "smol-rt")]
 use smol_macros::test;
 
-#[cfg(feature = "compio-rt")]
-use compio_fs::File;
-#[cfg(feature = "smol-rt")]
-use smol::fs::File;
-#[cfg(feature = "tokio-rt")]
-use tokio::fs::File;
-
 use crate::HttpBody;
 
-async fn do_test_file() -> Result<(), std::io::Error> {
+#[cfg(feature = "tokio-rt")]
+#[tokio::test]
+async fn test_file_tokio() -> Result<(), std::io::Error> {
+    use tokio::fs::File;
     let file = File::open("src/tests/files/index.html").await?;
-    let mut body = HttpBody::from_file(file);
+    let content = tokio_util::io::ReaderStream::new(file).map_ok(Frame::data);
+    let mut body = HttpBody::from_stream(content);
     let mut buffer = Vec::new();
     while let Some(Ok(chunk)) = body.next().await {
         if let Ok(chunk) = chunk.into_data() {
@@ -26,20 +24,28 @@ async fn do_test_file() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-#[cfg(feature = "tokio-rt")]
-#[tokio::test]
-async fn test_file() -> Result<(), std::io::Error> {
-    do_test_file().await
-}
-
 #[cfg(feature = "smol-rt")]
 #[apply(test!)]
-async fn test_file() -> Result<(), std::io::Error> {
-    do_test_file().await
+async fn test_file_smol() -> Result<(), std::io::Error> {
+    use smol::fs::File;
+    let file = File::open("src/tests/files/index.html").await?;
+    let content = file
+        .bytes()
+        .map_ok(|data| Frame::data(Bytes::copy_from_slice(&[data])));
+    let mut body = HttpBody::from_stream(content);
+    let mut buffer = Vec::new();
+    while let Some(Ok(chunk)) = body.next().await {
+        if let Ok(chunk) = chunk.into_data() {
+            buffer.extend_from_slice(&chunk);
+        }
+    }
+    assert_eq!(buffer, b"<html>\n<head>\n  <title>\n    Tested!\n  </title>\n</head>\n<body>\n  <p>\n    Tested!\n  </p>\n</body>\n</html>");
+    Ok(())
 }
 
 #[cfg(feature = "compio-rt")]
 #[compio::test]
-async fn test_file() -> Result<(), std::io::Error> {
+async fn test_file_compio() -> Result<(), std::io::Error> {
+    use compio_fs::File;
     do_test_file().await
 }
